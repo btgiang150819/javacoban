421286ab884a7fbf613465b00bc8ed37f73a8fd9
btgiang150819 <giang.buigiang.bui@hcmut.edu.vn>
2021-03-27 13:15:00 +0000

    update Main

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ public class Main {
         5: Câu điều kiện (ì else)
         {}: scope(phạm vi)
         */
-        int a = 6;
+       /* int a = 6;
         int b = 10;
         if(a > b){
             System.out.println(("A lớn hơn B"));
@@ -14,5 +14,14 @@ public class Main {
         else{
             System.out.println("A bé hơn hoặc bằng B");
         }
+        */
+        /*
+        int a = 5;
+        int b = 10;
+        if(a > b){
+            return;
+        }
+        System.out.print("Tiếp tục");
+        */
     }
 }
