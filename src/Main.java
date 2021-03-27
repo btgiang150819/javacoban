869bc466d5e44e212d00dc1280c489162d9904a1
btgiang150819 <giang.buigiang.bui@hcmut.edu.vn>
2021-03-27 13:07:22 +0000

    test

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public  static  void main(String[] args) throws Exception{
+        System.out.println("12334");
+        //http
+        /*
+        5: Câu điều kiện (ì else)
+        {}: scope(phạm vi)
+        */
+        int a = 6;
+        int b = 10;
+        if(a > b){
+            System.out.println(("A lớn hơn B"));
+        }
+        else{
+            System.out.println("A bé hơn hoặc bằng B");
+        }
+    }
+}
